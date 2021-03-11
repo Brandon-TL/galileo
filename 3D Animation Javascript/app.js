f41251cd8872a09f5167b71cfe7e6d2893fd04f3
Brandon <brandonmh@alumnos.iesgalileo.es>
2021-03-11 15:55:01 +0100

    Converse NF

--- 3D Animation Javascript/app.js
+++ 3D Animation Javascript/app.js
@@ -9,7 +9,7 @@ const purchase = document.querySelector('.purchase button');
 const description = document.querySelector('.info h3');
 const sizes = document.querySelector('.sizes');
 
-// Monving Animation Event
+//Moving Animation Event
 container.addEventListener("mousemove", (e) => {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
     let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
